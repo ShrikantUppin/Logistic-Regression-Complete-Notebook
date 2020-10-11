b809ee12b2fc60810ec4a613e08a9a45fb3bbc94
ShrikantUppin <shrikantuppin@gmail.com>
2020-10-11 22:04:03 +0530

    Deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+﻿web: sh setup.sh && streamlit ad_deployment.py
--- ad_deployment.py
+++ ad_deployment.py
@@ -1,10 +1,14 @@
+# Section 1: Import required Libraries...
 
 import streamlit as st
 import pickle
-import gzip
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
+
+#------------------------------------------------------------------
+
+# Section1: Unpickle all pickled data form jupyter notebook...
 
 infile1 = open('fianl_model.p','rb')
 final_model = pickle.load(infile1)
@@ -22,74 +26,92 @@ infile4 = open('country_city_dict.p','rb')
 country_city_dict = pickle.load(infile4)
 infile3.close()
 
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Section 2: From unpickled infile 4, Generating different dataframes for city & country names
+#            this will helpful during writing streamlit codes.
+
 city_df = pd.DataFrame(list(city_codes.items()), columns=['City','Code'], index=[i for i in range(1,len(city_codes)+1)])
 country_df = pd.DataFrame(list(country_codes.items()),columns=['Country','Code'], index=[i for i in range(1,len(country_codes)+1)])
 
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+#Section 3: Writing function which will return prediction:
+    
 def inputs(Daily_Time_Spent_on_Site, Age, Area_Income,Daily_Internet_Usage, City, Male, Country):
     
     new_data=pd.DataFrame({'Daily_Time_Spent_on_Site':Daily_Time_Spent_on_Site,"Age":Age,"Area_Income":Area_Income, "Daily_Internet_Usage":Daily_Internet_Usage,
                            "City":City, "Male":Male, "Country":Country},index=[1])
    
     new_data[["Daily_Time_Spent_on_Site","Area_Income","Daily_Internet_Usage"]] = new_data[["Daily_Time_Spent_on_Site","Area_Income","Daily_Internet_Usage"]].astype('float')
-    
     new_data[["Age","City","Male","Country"]] = new_data[["Age","City","Male","Country"]].astype('int')
-    
-
     final_predict = final_model.predict(new_data)
     
     return final_predict
     
-    
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------   
 
-def final():
+# Writing Streamlit webpage section ...this will displayed on web/taking inputs from user etc.
+
+def to_be_display():
     
-    st.title('Logistic Regression')
+    #--------------------------this will be displayed on main page-----------------------------
+    
+    st.title('Logistic Regression Model Deployment')
     '''## Predicting user has clicked on Advertise or not!'''
     '''Dataset : advertising.csv'''
     '''@ Shrikant Uppin'''
     '''***'''
+    #------------------------------------------------------------------------------------------
+    #-------------------------displayed in sidebar of webpage app------------------------------
+    # Writing treamlit code to take inputs from user & assign it to inputs() function.
     
     st.sidebar.title('Provide Inputs:')
-    # Daily_Time_Spent_on_Site
+    
     Daily_Time_Spent_on_Site = st.sidebar.number_input('Daily Time Spent in Minutes', 32, 92)
-    
-    
-    # Area Income
     Area_Income = st.sidebar.number_input('Area income in USD',10000,80000)
-    
-    # Daily_Internet_Usage
     Daily_Internet_Usage = st.sidebar.number_input('Daily Internet Usage',100, 300)
     
-    # Male
+    #---------------------------------------------------------------
+    # Writing mini-inside loop to assign value to Male variable.
+    
     male = st.sidebar.selectbox('Select Gender', ['Male','Female'])
     if male == 'Male':
         Male = 1
     else:
         Male =0
+    #---------------------------------------------------------------
     
-      # Age of the user
     Age = st.sidebar.number_input("Age of the user", 19, 60, 40)
     
-     # getting code value for entered country & reassigning to it Coutry variable in function.
+    # getting code value for entered country & reassigning to it Coutry variable in function.
+    
     country = st.sidebar.selectbox('Select Country', country_df.Country.unique())
     search1 = country
     a = country_df.loc[country_df.isin([search1]).any(axis=1)].index.values
     b = country_df.Code.loc[a].values
     Country = b
     
-    #Similar for cities..when selected particular country then automatic cities
-    #... in that country will be displayed to select.
-    
+    #--------------------------
     city = st.sidebar.selectbox('Select City', list(country_city_dict.get(search1)))
     c = city_df.loc[city_df.isin([city]).any(axis=1)].index.values
     d = city_df.Code.loc[c].values
     City = d
-    ''' ### Have you provided Inputs..? Lets predict Yes/No..!!!''' 
+    
+    #--------------------------this will be displayed on main page-----------------------------
+    ''' ### Want to check how ref. raw data looks..?'''
+    if st.button('Raw Data'):
+        raw_data = pd.read_csv('advertising.csv')
+        st.write(raw_data.head())
+    '''***'''
+    st.write()
+    ''' ### Have you provided Inputs..? Let's predict Yes/No..!!!''' 
     '''   '''
     if st.button('predict'):
         output = inputs(Daily_Time_Spent_on_Site, Age, Area_Income,Daily_Internet_Usage, City, Male, Country)
         if output ==1:
-            st.title('User clicked on Advertise..!!!')
+            st.write(' ### User clicked on Advertise..!!!')
             st.markdown("![Image](https://github.com/ShrikantUppin/Logistic-Regression-Complete-Notebook/blob/main/yes.gif?raw=true)")
         else:
             st.markdown("![Image](https://github.com/ShrikantUppin/Logistic-Regression-Complete-Notebook/blob/main/no.gif?raw=true)")
@@ -98,8 +120,14 @@ def final():
     link = '[Click here for source code..!](https://github.com/ShrikantUppin/Logistic-Regression-Complete-Notebook)'
     st.markdown(link, unsafe_allow_html=True)
     
+    '''***'''
+    
     link = '[Fallow me on GitHub](https://github.com/ShrikantUppin)'
     st.markdown(link, unsafe_allow_html=True)
     
-final()
+#------calling to_be_display() function------------------------
+
+to_be_display()
+
+#--------------------------------end of the code------------------------------------------------
     
